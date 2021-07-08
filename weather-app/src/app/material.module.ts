8de281f879cba1a7b9a2999981fcf64fb0469e3f
kaushikSuhas94 <kaushiksuhas94@gmail.com>
2021-07-08 23:40:42 +0530

    Step 3

--- weather-app/src/app/material.module.ts
+++ weather-app/src/app/material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 
 @NgModule({
-    imports: [MatFormFieldModule, MatInputModule, MatSidenavModule],
-    exports: [MatFormFieldModule, MatInputModule, MatSidenavModule]
+    imports: [MatFormFieldModule, MatInputModule, MatSidenavModule, MatDialog, MatDialogRef],
+    exports: [MatFormFieldModule, MatInputModule, MatSidenavModule, MatDialog, MatDialogRef]
 })
 export class MaterialModule {}
--- weather-app/src/app/welcome/welcome.component.ts
+++ weather-app/src/app/welcome/welcome.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  name: string;
+  location: string;
+}
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent  {
 
-  constructor() { }
+  location: string;
+  name: string;
 
-  ngOnInit(): void {
+  constructor(public dialog: MatDialog) { }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverview, {
+      width: '250px',
+      data: {name: this.name, location: this.location}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.location = result;
+    });
+  }
+}
+
+@Component({
+  selector: 'dialog-overview',
+  templateUrl: 'dialog-overview.html',
+})
+export class DialogOverview {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverview>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
