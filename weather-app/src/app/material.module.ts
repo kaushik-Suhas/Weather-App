3539803429dd3fee7db24e6829d220694785084e
kaushikSuhas94 <kaushiksuhas94@gmail.com>
2021-07-14 13:06:13 +0530

    Step 15

--- weather-app/src/app/material.module.ts
+++ weather-app/src/app/material.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from "@angular/core";
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatDialogModule} from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSelectModule} from '@angular/material/select';
-import {MatAutocompleteModule} from '@angular/material/autocomplete'
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatMenuModule } from '@angular/material/menu'
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import {MatAutocompleteModule} from '@angular/material/autocomplete'
               MatButtonModule, 
               MatIconModule,
               MatSelectModule,
-              MatAutocompleteModule],
+              MatAutocompleteModule,
+              MatMenuModule],
               
     exports: [MatFormFieldModule, 
               MatInputModule, 
@@ -26,7 +28,8 @@ import {MatAutocompleteModule} from '@angular/material/autocomplete'
               MatButtonModule, 
               MatIconModule,
               MatSelectModule,
-              MatAutocompleteModule]
+              MatAutocompleteModule,
+              MatMenuModule]
 })
 export class MaterialModule {
 
